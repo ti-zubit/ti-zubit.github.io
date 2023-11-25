0b26276286857351cc1fa8331a31dd94cc55e494
ti-zubit <t.mzlll37@gmail.com>
2023-11-25 14:56:20 +0900

    クエリ文字列調整

--- analysis.js
+++ analysis.js
@@ -1,65 +1,75 @@
-async function fetchDataAsync(yearmonth) {
-  await fetch('https://script.google.com/macros/s/AKfycbxEb2_mENr882g6KWBNLbbAJ7B5R8hvwZ1pIX0bpdyWIDfAnl_ZPQDuzRKqy3RV-8yS/exec' + "?yearmonth=" + yearmonth)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // JSONデータを取得する例
-  })
-  .then(data => {
-    generateTable(data.data, yearmonth); // サーバーから取得したデータを表示
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+async function fetchSurvivalDataAsync(yearmonth) {
+  let url = 'https://script.google.com/macros/s/AKfycbw6tJxQqIj4bnLqisHO510iEQkPDbNkSE857X7reZld5PJ9w4_jsNZ2PE8pxNHM_OJr/exec';
+  url += yearmonth ? "?yearmonth=" + yearmonth : ""; 
+  await fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // JSONデータを取得する例
+    })
+    .then(data => {
+      if (!data.data) {
+        console.log(data.error);
+      } else {
+        generateTable(data.data, yearmonth); // サーバーから取得したデータを表示
+      }
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
 
-  function generateTable(data, yearmonth) {
+async function fetchTemperatureDataAsync(place, month, year){
 
-    // <table>, <thead>, <tbody>作成
-    const tbl = document.createElement("table");
-    const tblHead = document.createElement("thead");
-    const tblBody = document.createElement("tbody");
+}
 
-    // ヘッダーを作成
-    const header = ["Sowing time", "Species", "Number of survivors", "Number of seeds sown"];
+function generateTable(data, yearmonth) {
+
+  // <table>, <thead>, <tbody>作成
+  const tbl = document.createElement("table");
+  const tblHead = document.createElement("thead");
+  const tblBody = document.createElement("tbody");
+
+  // ヘッダーを作成
+  const header = ["Sowing time", "Species", "Number of survivors", "Number of seeds sown"];
+  const row = document.createElement("tr");
+  header.forEach(element => {
+    const cell = document.createElement("th");
+    const cellText = document.createTextNode(element);
+    cell.appendChild(cellText);
+    row.appendChild(cell)
+  });
+  tblHead.appendChild(row);
+
+  for (const element of data) {
     const row = document.createElement("tr");
-    header.forEach(element => {
-      const cell = document.createElement("th");
-      const cellText = document.createTextNode(element);
+
+    const property_names = ["sowMonth", "species", yearmonth, "seedNum"];
+
+    for (let i = 0; i < property_names.length; i++) {
+      let cell;
+      if (i == 0) { cell = document.createElement("th") }
+      else { cell = document.createElement("td") };
+      const cellText = document.createTextNode(element[property_names[i]]);
       cell.appendChild(cellText);
-      row.appendChild(cell)
-    });
-    tblHead.appendChild(row);
+      row.appendChild(cell);
+    }
 
-    for (const element of data) {
-      const row = document.createElement("tr");
+    tblBody.appendChild(row);
 
-      const property_names = ["sowMonth", "species", yearmonth, "seedNum"];
+    tbl.setAttribute("border", "2");
 
-      for (let i = 0; i < property_names.length; i++) {
-        let cell;
-        if(i == 0) {cell = document.createElement("th")}
-        else {cell = document.createElement("td")};
-        const cellText = document.createTextNode(element[property_names[i]]);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
+  };
 
-      tblBody.appendChild(row);
+  tbl.appendChild(tblHead);
+  tbl.appendChild(tblBody);
 
-      tbl.setAttribute("border", "2");
+  document.getElementById("survialTable").appendChild(tbl);
 
-    };
+}
 
-    tbl.appendChild(tblHead);
-    tbl.appendChild(tblBody);
-
-    document.body.appendChild(tbl);
-
-  }
-
-  // クエリ文字列をオブジェクトに変換
+// クエリ文字列をオブジェクトに変換
 function parseQueryString(queryString) {
   const params = {};
   const queryStringWithoutQuestionMark = queryString.slice(1); // 先頭の ? を削除
@@ -72,17 +82,15 @@ function parseQueryString(queryString) {
   return params;
 }
 
-function setTitle(yearmonth){
+function setTitle(yearmonth) {
   document.getElementById("heading").textContent = "Analysis: " + yearmonth;
 }
 
-  const queryString = window.location.search;
-  const queryParams =parseQueryString(queryString);
+const queryString = window.location.search;
+const queryParams = parseQueryString(queryString);
 
-  let yearmonth;
-  if(queryParams.yearmonth) {
-    yearmonth = queryParams.yearmonth;
-
-    setTitle(yearmonth);
-    fetchDataAsync(yearmonth);
-  }
+let yearmonth = "";
+if (queryParams.year && queryParams.month) {
+  yearmonth = queryParams.year + "-" + queryParams.month;
+}
+fetchSurvivalDataAsync(yearmonth);
